3388bf44ba9a585db769fb3a45b06763a20a635c
ilyajav <ilia777824@gmail.com>
2025-01-12 21:20:45 +0300

    Fix Manager class object name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Manager financeManager = new Manager();
+        Manager manager = new Manager();
 
-        financeManager.runProgram();
+        manager.runProgram();
     }
 }
